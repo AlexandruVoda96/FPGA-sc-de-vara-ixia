00f337307735d8c934fd5345ad70fad33c5978e5
AlexandruVoda96 <alexandru.voda@yahoo.com>
2017-07-30 03:12:23 +0300

    organize

--- modul ps2/top.v
+++
@@ -1,8 +0,0 @@
-module top(input clk,scl,sda,
-output [6:0]OUT1,
-output [6:0]OUT2);
-wire [7:0]data;
-ps2 ps2(.clk(clk),.scl(scl),.sda(sda),.data_out(data));
-transcodor i0(.in(data[3:0]),.out(OUT1));
-transcodor i1(.in(data[7:4]),.out(OUT2));
-endmodule
